dfaffb6dcc1762e5350313d61d7fdb36e0b1d270
Ericson-Manalo <ericmana98@gmail.com>
2023-08-31 17:35:55 +0200

    restyling the scaffolding, (recalled via axios the upi of the list project)

--- src/pages/ProjectShow.vue
+++ src/pages/ProjectShow.vue
@@ -13,6 +13,35 @@ import SingleProject from './SingleProject.vue';
     export default {
         name: {
             SingleProject
+        },
+
+        data(){
+            return{
+                projects : [],
+                apiUrl : 'http://127.0.0.1:8000/api/projects'
+            }
+        },
+
+        methods:{
+            getProjects(){
+
+                axios.get(this.apiUrl, {
+                    params: {
+                }
+                })
+                .then((response) => {
+                console.log(response.data.results.data);
+                this.projects = response.data.results.data;
+                })
+                .catch(function (error) {
+                console.log(error);
+                })
+
+            }
+        },
+
+        created(){
+            this.getProjects();
         }
     }
 </script>
