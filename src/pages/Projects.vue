2837a280c0e63800d80488ca623ecfe0e6d7e00c
Ericson-Manalo <ericmana98@gmail.com>
2023-08-31 18:08:37 +0200

    filled up the projectshow using axios to call my api and to show all the projects

---
+++ src/pages/Projects.vue
@@ -0,0 +1,52 @@
+<template lang="">
+    <section>
+        <SingleProject/>
+    </section>
+</template>
+
+
+<script>
+import axios from 'axios';
+import SingleProject from '../components/SingleProject.vue';
+
+
+    export default {
+        name: {
+            SingleProject
+        },
+
+        data(){
+            return{
+                projects : [],
+                apiUrl : 'http://127.0.0.1:8000/api/projects'
+            }
+        },
+
+        methods:{
+            getProjects(){
+
+                axios.get(this.apiUrl, {
+                    params: {
+                }
+                })
+                .then((response) => {
+                console.log(response.data.results.data);
+                this.projects = response.data.results.data;
+                })
+                .catch(function (error) {
+                console.log(error);
+                })
+
+            }
+        },
+
+        created(){
+            this.getProjects();
+        }
+    }
+</script>
+
+
+<style lang="">
+    
+</style>
