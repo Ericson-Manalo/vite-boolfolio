476e15184defaa4ba210365b90b9b417f8c68dcd
Ericson-Manalo <ericmana98@gmail.com>
2023-08-31 18:08:01 +0200

    created with a v-for my items in li and using router-link to call pages when clicked

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,43 @@
 <template lang="">
-    <div>
-        header
-    </div>
+    <header>
+        <nav class="navbar" style="background-color: #e3f2fd;">
+            <ul>
+                <li v-for="link in links">
+                    <router-link :to="{ name: link.route }">
+                        {{ link.name }}
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+
 </template>
+
 <script>
-export default {
-    
-}
+    export default {
+        name: 'AppHeader',
+        data() {
+            return {
+                links : [
+                    {
+                        route: 'homepage',
+                        name: 'Homepage'
+                    },
+                    {
+                        route: 'about',
+                        name: 'About Us'
+                    },
+                    {
+                        route: 'project.index',
+                        name: 'Posts'
+                    },
+                ]
+            }
+        }
+    }
 </script>
+
 <style lang="">
     
 </style>
