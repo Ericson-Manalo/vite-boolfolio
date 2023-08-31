644d88c4b88d3a0493eeb047234a757e8b73fbd1
Ericson-Manalo <ericmana98@gmail.com>
2023-08-31 18:45:38 +0200

    creating the structure of all projects in file singleproject

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <article class="">
+        <h2>
+            {{ project.title }}
+        </h2>
+
+        <h5>
+            {{ project.description }}
+        </h5>
+
+    </article>
+</template>
+<script>
+export default {
+    name: 'SingleProject',
+    props: {
+        'project': Object
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
