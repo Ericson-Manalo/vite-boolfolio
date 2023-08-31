011f0a07729263434bc55c21d4b76f571f1360dd
Ericson-Manalo <ericmana98@gmail.com>
2023-08-31 19:12:04 +0200

    deleted because filled the wrong page

--- src/pages/ProjectShow.vue
+++ src/pages/ProjectShow.vue
@@ -3,50 +3,11 @@
         
     </div>
 </template>
-
-
 <script>
-import axios from 'axios';
-import SingleProject from './SingleProject.vue';
-
-
-    export default {
-        name: {
-            SingleProject
-        },
-
-        data(){
-            return{
-                projects : [],
-                apiUrl : 'http://127.0.0.1:8000/api/projects'
-            }
-        },
-
-        methods:{
-            getProjects(){
-
-                axios.get(this.apiUrl, {
-                    params: {
-                }
-                })
-                .then((response) => {
-                console.log(response.data.results.data);
-                this.projects = response.data.results.data;
-                })
-                .catch(function (error) {
-                console.log(error);
-                })
-
-            }
-        },
-
-        created(){
-            this.getProjects();
-        }
-    }
+export default {
+    
+}
 </script>
-
-
 <style lang="">
     
 </style>
